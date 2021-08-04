733d0cf3c160ce69602164fa15326e052607795c
Totò Volturno <info@kunigu.tech>
2021-08-04 14:44:04 +0200

    improved error handling, in code and in docs

--- src/pies/pies.controller.ts
+++ src/pies/pies.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Get, Param } from '@nestjs/common';
+import { Body, Get, NotFoundException, Param } from '@nestjs/common';
 import { Query } from '@nestjs/common';
 import { Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { PieDto } from './dto/pies.dto';
 import { PieEntity } from './entities/pie.entity';
 import { PiesService } from './pies.service';
@@ -13,6 +13,7 @@ export class PiesController {
   constructor(private readonly piesService: PiesService) {}
 
   @ApiOkResponse({type: PieEntity, isArray: true})
+  @ApiNotFoundResponse()
   @ApiQuery({name: 'name', required: false})
   @ApiQuery({name: 'address', required: false})
   @Get('all')
@@ -20,37 +21,40 @@ export class PiesController {
     try {
       return await this.piesService.getPies(name, address);
     } catch(error) {
-      return error;
+      throw new NotFoundException(error);
     }
   };
 
   @ApiOkResponse({type: PieEntity, isArray: false})
+  @ApiNotFoundResponse()
   @Get('address/:address')
   async getPieByAddress(@Param('address') address: string): Promise<PieEntity> {
     try {
       return await this.piesService.getPieByAddress(address);
     } catch(error) {
-      return error;
+      throw new NotFoundException(error);
     }
   }
 
   @ApiOkResponse({type: PieEntity, isArray: false})
+  @ApiNotFoundResponse()
   @Get('name/:name')
   async getPieByName(@Param('name') name: string): Promise<PieEntity> {
     try {
       return await this.piesService.getPieByName(name);
     } catch(error) {
-      return error;
+      throw new NotFoundException(error);
     }
   }
 
   @ApiCreatedResponse({type: PieEntity})
+  @ApiNotFoundResponse()
   @Post('create')
   async createPie(@Body() pie: PieDto): Promise<PieEntity> {
     try {
       return await this.piesService.createPie(pie);
     } catch(error) {
-      return error;
+      throw new NotFoundException(error);
     }
   }
 }
