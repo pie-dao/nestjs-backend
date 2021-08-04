c87810f366d257e5f8e695087dd5af329c782a87
Totò Volturno <info@kunigu.tech>
2021-08-04 17:12:33 +0200

    changed helloWorld for helloPieDao

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello from PieDAO!"', () => {
+      expect(appController.getHello()).toBe('Hello from PieDAO!');
     });
   });
 });
