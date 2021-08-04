5019531023009520b9b60ad3de4fbdef07bb31a4
Totò Volturno <info@kunigu.tech>
2021-08-04 10:29:34 +0200

    first Pies skeleton

--- src/pies/pies.module.ts
+++ src/pies/pies.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PiesController } from './pies.controller';
+import { PiesService } from './pies.service';
 
-@Module({})
+@Module({
+  controllers: [PiesController],
+  providers: [PiesService]
+})
 export class PiesModule {}
