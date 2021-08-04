b5af3265a35c54cea86732f4926e4861bba162bf
Totò Volturno <info@kunigu.tech>
2021-08-04 12:03:12 +0200

    added swagger for api docs and playgroun

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Swagger Section Start
+  // Docs and API Playground...
   const swaggerConfig = new DocumentBuilder()
     .setTitle('PieDAO API')
     .setDescription('The Official PieDAO Backend Engine')
@@ -13,6 +15,7 @@ async function bootstrap() {
 
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('/playground', app, swaggerDocument);
+  // Swagger Section End
 
   await app.listen(3000);
 }
