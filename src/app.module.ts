a09d6a2f52561e383926c8a01e3b29359a3148fc
Totò Volturno <info@kunigu.tech>
2021-08-04 17:03:03 +0200

    added netsj/config for dotenv handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PiesModule } from './pies/pies.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
+console.log(process.env.MONGO_DB);
 @Module({
   imports: [
     PiesModule,
-    MongooseModule.forRoot('mongodb+srv://heroku-app:4IGm1eTroN7iJ7GH@piedaobackendcluster.cmtix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB)
+    // 'mongodb+srv://heroku-app:4IGm1eTroN7iJ7GH@piedaobackendcluster.cmtix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   ],
   controllers: [AppController],
   providers: [AppService],
