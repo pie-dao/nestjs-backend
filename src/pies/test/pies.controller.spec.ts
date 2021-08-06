28acb87375fe5ee8efce0e302159fd1ce3ecd583
Totò Volturno <info@kunigu.tech>
2021-08-06 16:45:24 +0200

    completed first valid test for getPies - controller

--- src/pies/test/pies.controller.spec.ts
+++ src/pies/test/pies.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PieEntity } from '../entities/pie.entity';
 import { PiesController } from '../pies.controller';
 import { PiesService } from '../pies.service';
+import { PiesStub } from './stubs/pies.stubs';
 
 jest.mock('../pies.service');
 
@@ -16,9 +18,30 @@ describe('PiesController', () => {
     }).compile();
 
     controller = module.get<PiesController>(PiesController);
+    service = module.get<PiesService>(PiesService);
+
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getPies', () => {
+    describe('When getPies is called', () => {
+      let pies: PieEntity[];
+
+      beforeEach(async () => {
+        pies = await controller.getPies();
+      });
+
+      test('then it should call pieService', () => {
+        expect(service.getPies).toBeCalled();
+      });
+
+      test('then it should return an array of PieEntity', () => {
+        expect(pies).toEqual(PiesStub);
+      })
+    });
   });
 });
