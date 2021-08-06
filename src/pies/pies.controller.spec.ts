7afacc97ccf08e93677c148aedcd3c173f47db6d
Totò Volturno <info@kunigu.tech>
2021-08-06 15:51:51 +0200

    first approach to unit test files

--- src/pies/pies.controller.spec.ts
+++ src/pies/pies.controller.spec.ts
@@ -1,12 +1,28 @@
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PieHistoryEntity, PieHistorySchema } from './entities/pie-history.entity';
+import { PieEntity, PieSchema } from './entities/pie.entity';
 import { PiesController } from './pies.controller';
+import { PiesService } from './pies.service';
 
 describe('PiesController', () => {
   let controller: PiesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule,
+        ConfigModule.forRoot(),
+        ScheduleModule.forRoot(),        
+        MongooseModule.forRoot(process.env.MONGO_DB),
+        MongooseModule.forFeature([{ name: PieEntity.name, schema: PieSchema }]),
+        MongooseModule.forFeature([{ name: PieHistoryEntity.name, schema: PieHistorySchema }])        
+      ],      
       controllers: [PiesController],
+      providers: [PiesService],
     }).compile();
 
     controller = module.get<PiesController>(PiesController);
