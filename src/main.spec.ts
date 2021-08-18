8a877f1948405d1c27ee3c628c89b38b495e76f2
Totò Volturno <info@kunigu.tech>
2021-08-18 16:13:40 +0200

    improved test for main.ts

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import { response } from 'express';
 import * as request from 'supertest';
 import main from './main';
 
 describe("myApp", () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    app = await main;
+  });
+
   it('should implement CORS', async() => {
-    let app = await main;
     const { headers } = await request(app.getHttpServer()).get('/');
     expect(headers['access-control-allow-origin']).toEqual('*');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
