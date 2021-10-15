8370ef66b7e0c63c0621792bef75c6f03c73b7fd
Antonio Della Porta <antoniodellaporta.13@protonmail.com>
2021-10-15 12:44:53 +0200

    Adds types to Tasks service

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { HttpModule} from '@nestjs/axios';
-import { MerkleTree } from '../helpers/merkleTree/merkle-tree';
 
 @Module({
   imports: [
