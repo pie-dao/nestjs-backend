dc3167c7f5f3de12d36dda6567bfeb72cfa493a4
Totò Volturno <info@kunigu.tech>
2021-10-27 19:48:32 +0200

    added delegation feature

--- src/staking/types/staking.types.Participation.ts
+++ src/staking/types/staking.types.Participation.ts
@@ -3,6 +3,7 @@ import { Vote } from "./staking.types.Vote";
 
 export interface Participation {
     address: string;
+    delegatedTo: string;
     participation: number;
     staker: Staker;
     votes: Vote[];
